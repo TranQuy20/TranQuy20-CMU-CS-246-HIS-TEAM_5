ec34b261e879bded7da6a0b2d3182c0d2d71fdd4
TranQuy20 <162583817+TranQuy20@users.noreply.github.com>
2024-03-14 18:54:25 +0700

    Add files via upload

---
+++ CMU-CS 246 HIS - TEAM 5/2. TECHNICAL REVIEW/5. Rework/NguyenAnhHuy - TranViCuong_REWORK/TEAM 5 - CODE PART OF PROJECT/src/com/BackEnd/MainJava.java
@@ -0,0 +1,11 @@
+package com.BackEnd;
+
+import com.Main.Main;
+
+public class MainJava {
+    
+    public static void main(String[] args) {
+        Main run = new Main();
+        run.setVisible(true);
+    }
+}
