3237c902542684af331ea46d4c4c11dc4b4b9736
Nguyễn Anh Huy <162583530+NguyenAnhHuy06@users.noreply.github.com>
2024-04-04 17:15:09 +0700

    Add files via upload

---
+++ TEAM 5 - CODE PART OF PROJECT/src/com/BackEnd/Function.java
@@ -0,0 +1,45 @@
+package com.BackEnd;
+
+public class Function {
+
+    //public double sum;
+    public double num1;
+    public double num2;
+
+    public Function() {
+        num1 = 0;
+        num2 = 0;
+    }
+
+    public double AddictionNumber(double num1, double num2) {
+        return num1 + num2;
+
+    }
+
+    public double SubtractionNumber(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public double MultiplicationNumber(double num1, double num2) {
+        return num1 * num2;
+
+    }
+
+    public double DivisionNumber(double num1, double num2) {
+        return num1 / num2;
+
+    }
+
+    public double ModulusNumber(double num1, double num2) {
+        return num1 % num2;
+
+    }
+    
+    public double Square(double num1){
+        return Math.pow(num1, 2);
+    }
+    
+    public double SquareRoot(double num1){
+        return Math.sqrt(num1);
+    }
+}
